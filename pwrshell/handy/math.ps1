dcb3e8e0c5bfb63220e6cbc86f5131a70990489f
a1c43my <sk8flowtheengineer@outlook.com>
2022-04-24 00:01:50 -0500

    Create math.ps1

---
+++ pwrshell/handy/math.ps1
@@ -0,0 +1,8 @@
+#take a command and show the help
+# using param to make these cli friendly
+param(
+    [Parameter()]
+    [String]$f,[String]$x,[String]$y,
+    [String]$z,[String]$mean,[String]$var,
+    [String]$std,[String]$zscore
+)
