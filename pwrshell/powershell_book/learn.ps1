5ac97d65e36ea4e52677e6c302a183922a239a1b
a1c43my <sk8flowtheengineer@outlook.com>
2022-05-05 08:19:38 -0500

    busy busy

---
+++ pwrshell/powershell_book/learn.ps1
@@ -0,0 +1,47 @@
+<# 
+ working with some powershell concepts to try and make something useful
+
+ paramas and the try statements i need
+
+ for now the params are simply a file name and what you would like to do
+
+
+
+#>
+#first function
+ 
+
+param(
+    [Parameter()]
+    [String]$vic,[String]$port,[String]$file,[String]$super,[String]$site,[String]$count,[String]$m
+)
+function filecheck($file) {
+    Test-Path $file
+}
+
+function hostping($sitein,$c){
+    ping $sitein
+}
+
+
+function sitescan($sitein){
+    wget "http://$sitein"
+}
+
+
+if($m -eq "site" -and $count){
+    echo "sending ping to $vic"
+    sitescan($vic)
+    break
+}
+if($m -eq "site" -and !($count)){
+    echo "pinging nobody, you forgot the count"
+    filecheck("./test.txt")
+    break
+}
+if($m -eq "vic" -and $count){
+    echo "sending ping to $vic"
+    hostping($vic=$vic,$count=$count)
+    break
+}
+else {Write-Output " Usage /.learn.ps1 -m [site/vic] to see help []"}
