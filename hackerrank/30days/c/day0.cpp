94f5b0625199d7aec2c8d4646f2907209c2e3257
a1c43my <sk8flowtheengineer@outlook.com>
2022-05-06 09:26:58 -0500

    starting up
    work slowed down so i am working on some things i didnt have the chance to finish during my 100 days challenge, and uploading all the stuff i missed

---
+++ hackerrank/30days/c/day0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string readin; 
+    getline(cin, readin);
+
+    
+    cout << "Hello, World." << endl;
+
+    cout << readin << endl;
+
+    return 0;
+}
