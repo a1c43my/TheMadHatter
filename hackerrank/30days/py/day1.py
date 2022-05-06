0aa44392aca5dcb3200e7f074a2d6574be96f543
a1c43my <sk8flowtheengineer@outlook.com>
2022-05-06 09:50:32 -0500

    Update day1.py
    solved this one but i like adding notes

--- hackerrank/30days/py/day1.py
+++ hackerrank/30days/py/day1.py
@@ -4,9 +4,9 @@ s = 'HackerRank '
 # Declare second integer, double, and String variables.
 
 # Read and save an integer, double, and String to your variables.
-i2 = int(input())
-d2 = float(input())
-s2 = input()
+i2 = int(input()) # i used int to wrap the input
+d2 = float(input()) # float to do the same
+s2 = input() 
 # Print the sum of both integer variables on a new line.
 
 # Print the sum of the double variables on a new line.
