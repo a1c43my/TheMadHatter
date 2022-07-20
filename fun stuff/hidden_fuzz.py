93b15513ef32e22f64d0b116c4aee75635c580b9
a1c43my <sk8flowtheengineer@outlook.com>
2022-07-20 06:31:55 -0500

    Update hidden_fuzz.py

--- fun stuff/hidden_fuzz.py
+++ fun stuff/hidden_fuzz.py
@@ -14,8 +14,9 @@ def encryptData(buff):
 t = encryptData('doesntmatterthedata')
 y = str(t).replace('CREATED_BY\\pyAesCrypt 6.','').replace('AES','').replace('b','').replace('\'','').replace('\\x00','')
 yt = len(y)
-st = 0
+
 def fuxer():
+    st=0
     while True:
         print('fuzzinf with {} bytes'.format(len(y[0:st])))
         time.sleep(1)
